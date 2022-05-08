96d2dd75df2f7e5fd6c3d1661c6334e6d45284f8
kurtserdar <serdar.kurt@outlook.com>
2022-05-08 16:10:53 +0300

    logsigner.py v1.0

--- logsigner.py
+++ logsigner.py
@@ -1,54 +1,73 @@
 import sys
+import os.path
 from Cryptodome.PublicKey import RSA
 from Cryptodome.Hash import SHA256
 from Cryptodome.Signature import PKCS1_v1_5
 
 
+
 def usage():
     print("Usage: \n"
-          "digi-sig -s  <priv-key> <data> <signature-file> \n"
-          "digi-sig -v  <PUB-key> <data> <signature-file> \n")
+          "logsigner -s  <data> \n"
+          "logsigner -v  <data> \n")
 
-
-if (len(sys.argv) < 5):
+if (len(sys.argv) < 3):
     usage()
     quit()
 
+
+
 op = sys.argv[1]
-key_f = sys.argv[2]
-data_f = sys.argv[3]
-sig_f = sys.argv[4]
+data_f = sys.argv[2]
 
-
-def generate_signature(key, data, sig_f):
+def generate_signature(key, data):
     print("Generating Signature")
     h = SHA256.new(data)
     rsa = RSA.importKey(key)
     signer = PKCS1_v1_5.new(rsa)
     signature = signer.sign(h)
-    with open(sig_f, 'wb') as f: f.write(signature)
+    with open(data_f + "signed", 'wb') as f: f.write(signature)
 
 
-def verify_signature(key, data, sig_f):
+def verify_signature(key, data):
     print("Verifying Signature")
     h = SHA256.new(data)
     rsa = RSA.importKey(key)
     signer = PKCS1_v1_5.new(rsa)
-    with open(sig_f, 'rb') as f: signature = f.read()
+    with open(data_f + "signed", 'rb') as f: signature = f.read()
     rsp = "Success" if (signer.verify(h, signature)) else " Verification Failure"
     print(rsp)
 
+signCertExists = os.path.isfile('sign.pem')
+verifyCertExists = os.path.isfile('verify.pem')
+
+# print(signCertExists)
+# print(verifyCertExists)
+
+# check pem files are exists if not create them
+if (verifyCertExists!= True) or (signCertExists != True):
+    key = RSA.generate(1024)
+    privKeyString = key.export_key()
+
+    with open ("sign.pem","w") as prvFile:
+        print("{}".format(privKeyString.decode()),file=prvFile)
+
+    pubKeyString = key.publickey().exportKey()
+    with open ("verify.pem", "w") as pubFile:
+            print("{}".format(pubKeyString.decode()), file=pubFile)
 
 # Read all file contents
-with open(key_f, 'rb') as f: key = f.read()
+with open("sign.pem", 'rb') as f: key = f.read()
 with open(data_f, 'rb') as f: data = f.read()
 
 if (op == "-s"):
     # Generate Signature
-    generate_signature(key, data, sig_f)
+    generate_signature(key, data)
 elif (op == "-v"):
     # Verify Signature
-    verify_signature(key, data, sig_f)
+    verify_signature(key, data)
 else:
     # Error
     usage()
+    
+
