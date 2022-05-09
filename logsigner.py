5974811b17d0d08952ff6d661480adb038a7bcf2
kurtserdar <serdar.kurt@outlook.com>
2022-05-09 09:34:34 +0300

    logsigner.py v1

--- logsigner.py
+++ logsigner.py
@@ -19,6 +19,41 @@ if (len(sys.argv) < 3):
 op = sys.argv[1]
 data_f = sys.argv[2]
 
+"""def get_all_file_paths(directory):
+  
+    # initializing empty file paths list
+    file_paths = []
+  
+    # crawling through directory and subdirectories
+    for root, directories, files in os.walk(directory):
+        for filename in files:
+            # join the two strings in order to form the full filepath.
+            filepath = os.path.join(root, filename)
+            file_paths.append(filepath)
+  
+    # returning all file paths
+    return file_paths
+
+def main():
+    # path to folder which needs to be zipped
+    directory = './python_files'
+  
+    # calling function to get all file paths in the directory
+    file_paths = get_all_file_paths(directory)
+  
+    # printing the list of all files to be zipped
+    print('Following files will be zipped:')
+    for file_name in file_paths:
+        print(file_name)
+  
+    # writing files to a zipfile
+    with ZipFile('my_python_files.zip','w') as zip:
+        # writing each file one by one
+        for file in file_paths:
+            zip.write(file)
+  
+    print('All files zipped successfully!')   """      
+
 def generate_signature(key, data):
     print("Generating Signature")
     h = SHA256.new(data)
@@ -26,6 +61,7 @@ def generate_signature(key, data):
     signer = PKCS1_v1_5.new(rsa)
     signature = signer.sign(h)
     with open(data_f + ".signed", 'wb') as f: f.write(signature)
+    print("Done")
 
 
 def verify_signature(key, data):
@@ -68,5 +104,3 @@ elif (op == "-v"):
 else:
     # Error
     usage()
-    
-
